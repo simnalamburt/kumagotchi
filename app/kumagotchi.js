349ada220f2a804c02adb0e58df485c74c700a2c
Hyeon Kim <simnalamburt@gmail.com>
2016-06-16 03:04:08 +0900

    바벨 붙임

--- app/kumagotchi.js
+++ app/kumagotchi.js
@@ -1,5 +1,9 @@
 const React = require('react');
 const { render } = require('react-dom');
 
-console.log(React);
-console.log(render);
+render(
+  <div id="screen">
+    <img className="blink" src="res/2.png"/>
+  </div>,
+  document.getElementById('target')
+);
