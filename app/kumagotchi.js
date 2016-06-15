19a759a3fe22b731646c4a47d5c17ad3c39e4c36
Hyeon Kim <simnalamburt@gmail.com>
2016-06-16 03:34:28 +0900

    리액트 라우트

--- app/kumagotchi.js
+++ app/kumagotchi.js
@@ -1,9 +1,24 @@
 const React = require('react');
 const { render } = require('react-dom');
+const { Router, Route, Link, hashHistory } = require('react-router');
+
+const Front = _ => (
+  <div id="front">
+    <Link to="/main"><img className="blink" src="res/2.png"/></Link>
+  </div>
+);
+
+const Main = _ => (
+  <div id="main">
+    <h1>Hello, world!</h1>
+    <p>ㅇㅅㅇ)/</p>
+  </div>
+);
 
 render(
-  <div id="screen">
-    <img className="blink" src="res/2.png"/>
-  </div>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Front}/>
+    <Route path="/main" component={Main}/>
+  </Router>,
   document.getElementById('target')
 );
